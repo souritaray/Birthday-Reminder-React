42852af127616d60b79b776da4ccb13335cd0bdb
souritaray <100030958+souritaray@users.noreply.github.com>
2023-05-15 08:00:02 +0530

    added new functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import { useState } from "react";
-import data from "./data";
-import List from "./List";
-function App(){
-    const[detail,setDetail]=useState(data);
-    
-    return(
-        <main className="container"> 
-<h3>{detail.length} Birthdays today: </h3>
-<List detail={detail} > </List>
-<button onClick={()=>{setDetail([])}}> clear all</button>
-        </main>
-        
-    )
-
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+function App() {
+  const [people, setPeople] = useState(data)
+  const removeFunc=(id)=>{
+    let newppl=people.filter((p)=>
+        p.id !== id);
+        setPeople(newppl);
+    }
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        {people.map((person) => {
+        return (
+          <List person={person} btn={<button onClick={()=>{removeFunc(person.id)}}>remove</button>}/>
+        );
+      })}
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  )
 }
 export default App;
