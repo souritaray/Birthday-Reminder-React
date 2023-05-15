9232987f6d982e890064c20287bafcfc3fed840c
Sourita Ray <souritaray@gmail.com>
2023-05-15 08:58:27 +0530

    passing a function as prop

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import { useState } from "react";
-import data from "./data";
-import List from "./List";
-function App(){
-    const[detail,setDetail]=useState(data);
-    
-    return(
-        <main className="container"> 
-<h3>{detail.length} Birthdays today: </h3>
-<List detail={detail} > </List>
-<button onClick={()=>{setDetail([])}}> clear all</button>
-        </main>
-        
-    )
-
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
+function App() {
+  const [people, setPeople] = useState(data)
+  const removeFunc=(id)=>{
+    let newppl=people.filter((person)=>
+        person.id !== id);
+        setPeople(newppl);
+    }
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        {people.map((person) => {
+        return (
+          <List person={person} removeFunc={removeFunc}/>
+        );
+      })}
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  )
 }
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,25 +1,16 @@
-import { useState } from "react";
-function List({detail}){
-    const[store,setStore]=useState("");
-return(
+import React from 'react';
+const List = ({ person,removeFunc}) => {
+  return (
     <>
-    {detail.map((person)=>{
-       
-        
-return(
-    
-<div className="person" key={person.id}>
-    <img src={person.image}  />
+          <article key={person.id} className='person'>
+            <img src={person.image} alt={person.name} />
             <div>
               <h4>{person.name}</h4>
               <p>{person.age} years</p>
-               
+             <button onClick={()=>{removeFunc(person.id)}}>remove</button>
             </div>
-       
-    </div>
-)
-    })}
-  </>  
-)
-}
+          </article>
+    </>
+  );
+};
 export default List;
